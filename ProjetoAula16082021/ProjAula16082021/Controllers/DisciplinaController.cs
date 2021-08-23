11835d6f9190590c80f7dccb64a4358b1be5cb61
Lucas <Lucas@DESKTOP-U3D5K24>
2021-08-23 19:52:18 -0300

    Aula03

---
+++ ProjetoAula16082021/ProjAula16082021/Controllers/DisciplinaController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjAula16082021.Controllers
+{
+    public class DisciplinaController : Controller
+    {
+        // GET: Disciplina
+        public ActionResult Index()
+        {
+            DBAula16082021Entities context = new DBAula16082021Entities();
+            List<Disciplina> lstDisciplina = context.Disciplina.ToList<Disciplina>();
+
+            return View(lstDisciplina);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Disciplina disciplina)
+        {
+            if (ModelState.IsValid)
+            {
+                DBAula16082021Entities contextDisciplina = new DBAula16082021Entities();
+                contextDisciplina.Disciplina.Add(disciplina);
+                contextDisciplina.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(disciplina);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Disciplina disciplina = new Disciplina();
+            disciplina = new DBAula16082021Entities().Disciplina.First(i => i.Id == id);
+
+            return View(disciplina);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Disciplina disciplina)
+        {
+            if (ModelState.IsValid)
+            {
+                int id = disciplina.Id;
+                DBAula16082021Entities contextDisciplina = new DBAula16082021Entities();
+                Disciplina updatedDisciplina = contextDisciplina.Disciplina.First(d => d.Id == id);
+
+                updatedDisciplina.Id = disciplina.Id;
+                updatedDisciplina.Descricao = disciplina.Descricao;
+                updatedDisciplina.QtdAlunos = disciplina.QtdAlunos;
+                updatedDisciplina.NomeProfessor = disciplina.NomeProfessor;
+
+                contextDisciplina.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(disciplina);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Disciplina disciplina = new Disciplina();
+            disciplina = new DBAula16082021Entities().Disciplina.First(i => i.Id == id);
+
+            return View(disciplina);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            DBAula16082021Entities contextDisciplina = new DBAula16082021Entities();
+            Disciplina disciplina = new Disciplina();
+            disciplina = new DBAula16082021Entities().Disciplina.First(i => i.Id == id);
+
+            return View(disciplina);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirm(int id)
+        {
+            DBAula16082021Entities contextDisciplina = new DBAula16082021Entities();
+            Disciplina disciplina = new Disciplina();
+
+            disciplina = contextDisciplina.Disciplina.First(i => i.Id == id);
+            contextDisciplina.Disciplina.Remove(disciplina);
+            contextDisciplina.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ProjetoAula16082021/ProjAula16082021/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjAula16082021.MvcApplication" Language="C#" %>
