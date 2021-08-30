53ed910d353242f140f818b16f1fe17118787ef4
Lucas <Lucas@DESKTOP-U3D5K24>
2021-08-30 20:54:10 -0300

    aula3008

---
+++ Projeto30082021/Projeto30082021/Data/Projeto30082021Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Projeto30082021.Model;
+
+namespace Projeto30082021.Data
+{
+    public class Projeto30082021Context : DbContext
+    {
+        public Projeto30082021Context (DbContextOptions<Projeto30082021Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Projeto30082021.Model.Person> Person { get; set; }
+    }
+}
